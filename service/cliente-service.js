9afc74d5a05e7159c4b21cd338f5792ec207f444
tatialveso <tatiialveso@gmail.com>
2021-02-08 09:34:27 -0300

    :sparkles: create register client

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -5,6 +5,23 @@ const listaClientes = () => {
         })
 }
 
+const criarCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(response => {
+        return response.body
+    })
+}
+
 export const clienteService = {
-    listaClientes
+    listaClientes,
+    criarCliente
 }
